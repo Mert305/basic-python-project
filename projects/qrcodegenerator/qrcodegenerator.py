d0c4035de0b398e130f1ac7037a4d4ab697d4767
Mert305 <77837007+Mert305@users.noreply.github.com>
2022-11-19 22:33:41 +0300

    Delete projects directory

--- projects/qrcodegenerator/qrcodegenerator.py
+++
@@ -1,3 +0,0 @@
-import pyqrcode
-qr = pyqrcode.create("https://www.tiktok.com/@sibergazete") #?We get the link you want to go to  # Buraya Gitmek İstediğin Linki Alıyoruz
-qr.png('save.png', scale=8)
