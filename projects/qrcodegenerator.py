99e549610b66da52d471ef855e9cdded2d5f9916
Mert305 <77837007+Mert305@users.noreply.github.com>
2022-11-19 22:31:49 +0300

    Delete qrcodegenerator.py

--- projects/qrcodegenerator.py
+++
@@ -1,3 +0,0 @@
-import pyqrcode
-qr = pyqrcode.create("https://www.tiktok.com/@sibergazete") #?We get the link you want to go to  # Gitmek istediğiniz bağlantıyı yapıştırın
-qr.png('save.png', scale=8)
