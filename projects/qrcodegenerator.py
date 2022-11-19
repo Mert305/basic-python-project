95968b1b3c6d0db433eef6fe7e43d92b5a0755b2
Mert305 <77837007+Mert305@users.noreply.github.com>
2022-11-19 14:34:18 +0300

    Update qrcodegenerator.py

--- projects/qrcodegenerator.py
+++ projects/qrcodegenerator.py
@@ -1,3 +1,3 @@
 import pyqrcode
-qr = pyqrcode.create("https://www.tiktok.com/@sibergazete") #?We get the link you want to go to  # Buraya Gitmek İstediğin Linki Alıyoruz
+qr = pyqrcode.create("https://www.tiktok.com/@sibergazete") #?We get the link you want to go to  # Gitmek istediğiniz bağlantıyı yapıştırın
 qr.png('save.png', scale=8)
