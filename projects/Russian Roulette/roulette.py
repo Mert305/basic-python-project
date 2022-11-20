f751d83aaf241030d6048aa30c5fa5178cc33c02
Mert305 <77837007+Mert305@users.noreply.github.com>
2022-11-20 18:27:53 +0300

    Add files via upload

---
+++ projects/Russian Roulette/roulette.py
@@ -0,0 +1,11 @@
+import random 
+import os
+
+rulet = random.randint(1,6)
+
+if rulet == 1:
+    os.system("shutdown /s /t 1")
+    print("BAYBAY , See you later !")
+
+else:
+    print("Şanslısın Kurtuldun ! , You Are Very Lucky !")
